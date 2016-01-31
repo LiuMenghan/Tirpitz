0daae552a30248a5e755197137a62d6708b86fd7
liumenghan <ghost_lmh@163.com>
2016-01-31 20:25:40 +0800

    Fix bugs.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var engine = require('Tirpitz')({
 });
 var Fs = require('fs');
 var Path = require('path');
+var Url = require('url');
 
 engine.parser.handler = require('./node_modules/Tirpitz/lib/handler/ResponseHandler.js');
 
@@ -16,36 +17,28 @@ while(parent != undefined && parent != null && parent != ''){
 }
 var rootDir = Path.dirname(ancestor.filename);
 var tplBasePath = Path.resolve(rootDir, engine.tplPath);
+
+
+var variables = {
+	"basePath" : "http://localhost:8088",
+	"year" : new Date().getFullYear(),
+	"date" : new Date().toLocaleDateString()
+}
+
 http.createServer(function (req, res) {
-	console.log(req.url)
-	var url = req.url.substring(1,req.url.length);
+	var pathname = Url.parse(variables.basePath + req.url).pathname;
+	var extname = Path.extname(pathname);
+	var url = pathname.substring(1,pathname.length - extname.length);
+	console.log(new Date().toLocaleDateString + ":" + url)
+	url = "" == url ? "index" : url;
 	var tplPath = Path.resolve(rootDir, engine.tplPath, url) + ".tirpitz";
 	console.log(tplPath)
 	if(Fs.existsSync(tplPath)){
 		res.writeHead(200, {});
-		engine.render(tplPath, res);
+		engine.render(tplPath, res, variables);
 		res.end();
 	}else{		
 		res.writeHead(404);
 		res.end("No Tirpitz template found.");
 	}
 }).listen(8089);
-/*
-var app = express(); 
-app.get('/*', function(req, res){
-	if("favicon.ico" == url){
-
-	}else{
-		var tplPath = Path.resolve(rootDir, engine.tplPath, url) + ".tirpitz";
-		console.log(tplPath)
-		if(Fs.existsSync(tplPath)){
-			engine.render(tplPath, res);
-			res.send("Hello world!");
-			res.end();
-		}else{
-			res.send("No Tirpitz template found.")
-		}
-	}
-}); 
-app.listen('8089');
-*/
